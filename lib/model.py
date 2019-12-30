bbba9f925919c327e29dbe6a1222908604b1f26f
Narayan Acharya <narayan.acharya6@gmail.com>
2019-12-30 13:30:35 -0500

    Adds lib files to git

---
+++ lib/model.py
@@ -0,0 +1,183 @@
+# inbuilt lib imports:
+from typing import Dict
+import math
+
+# external libs
+import tensorflow as tf
+from tensorflow.keras import models, layers
+
+# project imports
+
+
+class CubicActivation(layers.Layer):
+    """
+    Cubic activation as described in the paper.
+    """
+    def call(self, vector: tf.Tensor) -> tf.Tensor:
+        """
+        Parameters
+        ----------
+        vector : ``tf.Tensor``
+            hidden vector of dimension (batch_size, hidden_dim)
+
+        Returns tensor after applying the activation
+        """
+        # TODO(Students) Start
+        return tf.math.pow(vector, 3)
+        # TODO(Students) End
+
+
+class DependencyParser(models.Model):
+    def __init__(self,
+                 embedding_dim: int,
+                 vocab_size: int,
+                 num_tokens: int,
+                 hidden_dim: int,
+                 num_transitions: int,
+                 regularization_lambda: float,
+                 trainable_embeddings: bool,
+                 activation_name: str = "cubic") -> None:
+        """
+        This model defines a transition-based dependency parser which makes
+        use of a classifier powered by a neural network. The neural network
+        accepts distributed representation inputs: dense, continuous
+        representations of words, their part of speech tags, and the labels
+        which connect words in a partial dependency parse.
+
+        This is an implementation of the method described in
+
+        Danqi Chen and Christopher Manning.
+        A Fast and Accurate Dependency Parser Using Neural Networks. In EMNLP 2014.
+
+        Parameters
+        ----------
+        embedding_dim : ``str``
+            Dimension of word embeddings
+        vocab_size : ``int``
+            Number of words in the vocabulary.
+        num_tokens : ``int``
+            Number of tokens (words/pos) to be used for features
+            for this configuration.
+        hidden_dim : ``int``
+            Hidden dimension of feedforward network
+        num_transitions : ``int``
+            Number of transitions to choose from.
+        regularization_lambda : ``float``
+            Regularization loss fraction lambda as given in paper.
+        trainable_embeddings : `bool`
+            Is the embedding matrix trainable or not.
+        """
+        super(DependencyParser, self).__init__()
+        self._regularization_lambda = regularization_lambda
+
+        if activation_name == "cubic":
+            self._activation = CubicActivation()
+        elif activation_name == "sigmoid":
+            self._activation = tf.keras.activations.sigmoid
+        elif activation_name == "tanh":
+            self._activation = tf.keras.activations.tanh
+        else:
+            raise Exception(f"activation_name: {activation_name} is from the known list.")
+
+        # Trainable Variables
+        # TODO(Students) Start
+        self.trainable_embeddings = trainable_embeddings
+        self.embeddings = tf.Variable(tf.random.uniform((vocab_size, embedding_dim), minval=-0.01, maxval=0.01),
+                                      trainable=trainable_embeddings)
+        self.w1 = tf.Variable(tf.initializers.GlorotNormal()((num_tokens * embedding_dim, hidden_dim)), trainable=True)
+        self.b1 = tf.Variable(tf.zeros(hidden_dim), trainable=True)
+
+        self.w2 = tf.Variable(tf.initializers.GlorotNormal()((hidden_dim, num_transitions)), trainable=True)
+        # TODO(Students) End
+
+    def call(self,
+             inputs: tf.Tensor,
+             labels: tf.Tensor = None) -> Dict[str, tf.Tensor]:
+        """
+        Forward pass of Dependency Parser.
+
+        Parameters
+        ----------
+        inputs : ``tf.Tensor``
+            Tensorized version of the batched input text. It is of shape:
+            (batch_size, num_tokens) and entries are indices of tokens
+            in to the vocabulary. These tokens can be word or pos tag.
+            Each row corresponds to input features a configuration.
+        labels : ``tf.Tensor``
+            Tensor of shape (batch_size, num_transitions)
+            Each row corresponds to the correct transition that
+            should be made in the given configuration.
+
+        Returns
+        -------
+        An output dictionary consisting of:
+        logits : ``tf.Tensor``
+            A tensor of shape ``(batch_size, num_transitions)`` representing
+            logits (unnormalized scores) for the labels for every instance in batch.
+        loss : ``tf.float32``
+            If input has ``labels``, then mean loss for the batch should
+            be computed and set to ``loss`` key in the output dictionary.
+
+        """
+        # TODO(Students) Start
+        embedded_tokens = tf.nn.embedding_lookup(self.embeddings, inputs)
+        embedded_tokens_reshaped = tf.reshape(embedded_tokens, [tf.shape(inputs)[0], -1])
+
+        weight_mul = tf.matmul(embedded_tokens_reshaped, self.w1)
+        result = tf.math.add(weight_mul, self.b1)
+        hidden_layer = self._activation(result)
+
+        logits = tf.matmul(hidden_layer, self.w2)
+        # TODO(Students) End
+
+        output_dict = {"logits": logits}
+
+        if labels is not None:
+            output_dict["loss"] = self.compute_loss(logits, labels)
+        return output_dict
+
+    def compute_loss(self, logits: tf.Tensor, labels: tf.Tensor) -> tf.float32:
+        """
+        Parameters
+        ----------
+        logits : ``tf.Tensor``
+            A tensor of shape ``(batch_size, num_transitions)`` representing
+            logits (unnormalized scores) for the labels for every instance in batch.
+
+        Returns
+        -------
+        loss : ``tf.float32``
+            If input has ``labels``, then mean loss for the batch should
+            be computed and set to ``loss`` key in the output dictionary.
+
+        """
+        # TODO(Students) Start
+        # Loss
+        keep_mask = tf.where(tf.greater_equal(labels, 0), 1.0, 0.0)
+
+        max = tf.math.reduce_max(logits, axis=1)
+        stable_logits = tf.math.subtract(logits, tf.expand_dims(max, axis=len(tf.shape(max))))
+        numerator = tf.math.exp(stable_logits)
+        numerator_corrected = tf.math.multiply(keep_mask, numerator)
+        denominator = tf.reshape(tf.math.reduce_sum(numerator_corrected, axis=1), (-1, 1))
+        softmax_logits = numerator_corrected / denominator
+
+        labels_masked = tf.math.multiply(labels, keep_mask)
+
+        loss = tf.reduce_mean(-tf.reduce_sum(tf.math.multiply(tf.dtypes.cast(labels_masked, dtype=tf.float32),
+                                                              tf.math.log(softmax_logits + 1e-9)), axis=1))
+
+        # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels_masked,
+        #                                                               tf.math.multiply(logits, keep_mask)))
+
+        # Regularization
+        l2 = tf.nn.l2_loss(self.w1) + tf.nn.l2_loss(self.b1) + tf.nn.l2_loss(self.w2)
+        if self.trainable_embeddings:
+            l2 += tf.nn.l2_loss(self.embeddings)
+
+        # The division by 2 in the paper is factored in the l2_loss function in TensorFlow!
+        regularization = self._regularization_lambda * l2
+
+        # TODO(Students) End
+        return loss + regularization
+
